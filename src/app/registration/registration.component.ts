ca005eb6bd28957a30f7ac618d3dd9d20bd947b5
potapovnr <potapovnr@gmail.com>
2019-11-13 14:44:23 +0300

    Добавлено уведомление при регистрации

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -2,6 +2,8 @@ import {Component, OnInit} from '@angular/core';
 import {User} from '../users/users';
 import {UserService} from '../users/user.service';
 import {Role} from '../enum/role';
+import {MatSnackBar} from '@angular/material';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-registration',
@@ -12,7 +14,8 @@ export class RegistrationComponent implements OnInit {
 
   createUser: User;
 
-  constructor(private userService: UserService) {
+  constructor(private userService: UserService, private snackBar: MatSnackBar,
+              private router: Router) {
   }
 
   ngOnInit() {
@@ -22,7 +25,11 @@ export class RegistrationComponent implements OnInit {
 
   public createUserFunc(user: User) {
     user.roleId = Role.USER;
-    this.userService.registerNewUser(user).subscribe();
+    this.userService.registerNewUser(user).subscribe(r => {
+      this.snackBar.open('Пользователь успешно создан', null, {duration: 2000});
+      this.router.navigate(['/login']);
+    });
+
   }
 
 }
